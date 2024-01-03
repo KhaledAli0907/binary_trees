91d9a7a072127f38b993f1f9cd66c23de6d9dee5
khaledali0907 <khaledali0907@gmail.com>
2024-01-03 15:15:22 +0200

    task#0-edit

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -2,7 +2,7 @@
 
 /**
  * binary_tree_node - Creates binary tree node
- * 
+ *
  * @parent: pointer to the parent node to create
  * @value: value to be inserted in this node
  *
@@ -14,7 +14,8 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	binary_tree_t *node = NULL;
 
 	node = malloc(sizeof(binary_tree_t));
-	if (!node) return NULL;
+	if (!node)
+		return (NULL);
 
 	node->parent = parent;
 	node->n = value;
