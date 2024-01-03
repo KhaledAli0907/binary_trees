6d4fe92908efa63141f48cabb7982cfbca9dcc84
khaledali0907 <khaledali0907@gmail.com>
2024-01-03 15:34:17 +0200

    task#1

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -6,25 +6,24 @@
  * @value: value of the node
  * Return: NULL if it fails or the new node
  */
+
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new_node;
+	binary_tree_t *node = NULL;
 
-	if (parent == NULL)
-	{
+	if (!parent)
 		return (NULL);
-	}
 
-	new_node = binary_tree_node(parent, value);
-	if (new_node == NULL)
-	{
+	node = binary_tree_node(parent, value);
+
+	if (node == NULL)
 		return (NULL);
-	}
+
 	if (parent->left != NULL)
 	{
-		new_node->left = parent->left;
-		parent->left->parent = new_node;
+		node->left = parent->left;
+		parent->left->parent = node;
 	}
-	parent->left = new_node;
-	return (new_node);
+	parent->left = node;
+	return (node);
 }
