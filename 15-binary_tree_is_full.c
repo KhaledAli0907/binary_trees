0bbe7039466efaad52775caaf0f5aef3dbd2f3ec
khaledali0907 <khaledali0907@gmail.com>
2024-01-03 23:11:04 +0200

    task#15-edit

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -17,9 +17,7 @@ int binary_tree_is_full(const binary_tree_t *tree)
 	if (!tree->right && !tree->left)
 		return (1);
 
-	if (tree->right != NULL && tree->left != NULL)
-		return (binary_tree_is_full(tree->right)
-				&& binary_tree_is_full(tree->right));
+	if (tree->right && tree->left)
+		return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
 	return (0);
 }
-
