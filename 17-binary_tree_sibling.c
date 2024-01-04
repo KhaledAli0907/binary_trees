0d3a87f07da50485c46a6666422fd6a4a021fe38
khaledali0907 <khaledali0907@gmail.com>
2024-01-04 11:35:35 +0200

    task#17

---
+++ 17-binary_tree_sibling.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_sibling - check if the node has a sibling or not
+ * @node: pointer to the node to be checked
+ * Return: pointer to the sibling node, NULL otherwise
+ */
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	if (!node->parent || !node)
+		return (NULL);
+
+	/* know if node is left or right child */
+	if (node->parent->left == node)
+		return (node->parent->right);
+
+	return (node->parent->left);
+}
